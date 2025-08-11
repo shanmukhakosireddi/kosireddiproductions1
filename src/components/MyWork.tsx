52192f755967d1fed9d2964561f235365e95e2be
shanmukhakosireddi <kosireddishanmukha@gmail.com>
2025-08-11 11:10:15 +0530

    Add Custom Image to Work Section

--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -16,8 +16,10 @@ const MyWork = () => {
         <div className="mb-20">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
             {[1, 2, 3, 4].map((index) => {
-              // Use custom image for first item, Pexels for others
+              // Use custom images for first two items, Pexels for others
               const imageUrl = index === 1 
+                ? '/Screenshot 2025-08-06 232725.png'
+                : index === 2
                 ? 'https://photos.fife.usercontent.google.com/pw/AP1GczMvgpbZu5wXpnCyiyMjngNg9VlWX68JSt7ND8lpy60OI5G2Br5eRy9e5A=w1216-h913-s-no-gm?authuser=0'
                 : `https://images.pexels.com/photos/${
                     index === 2 ? '3184418' : 
